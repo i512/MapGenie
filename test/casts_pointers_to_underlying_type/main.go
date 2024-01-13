12f09634ae113c031ef54d7d878ace8f2012467d
Морозов Илья Артурович <ilya.morozov@sbermarket.ru>
2024-01-13 22:29:34 +0300

    expand casts pointers to underlying type tests

--- test/casts_pointers_to_underlying_type/main.go
+++ test/casts_pointers_to_underlying_type/main.go
@@ -1,15 +1,32 @@
 package casts_pointers_to_underlying_type
 
 type Int int
+type IntPtr *int
 
 type A struct {
 	PtrToPtr *int
 	PtrToVal *int
 	ValToPtr int
+
+	StrPtrToByte *string
+	ByteToStrPtr []byte
+	StrToBytePtr string
+	BytePtrToStr *[]byte
+
+	StrPtrToBytePtr *string
+	BytePtrToStrPtr *[]byte
 }
 
 type B struct {
 	PtrToPtr *Int
 	PtrToVal Int
 	ValToPtr *Int
+
+	StrPtrToByte []byte
+	ByteToStrPtr *string
+	StrToBytePtr *[]byte
+	BytePtrToStr string
+
+	StrPtrToBytePtr *[]byte
+	BytePtrToStrPtr *string
 }
