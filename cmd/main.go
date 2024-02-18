090a2454583062ce94524651f186fa1110e333a1
Ilya Morozov <weugek@gmail.com>
2024-02-18 23:43:11 +0300

    add cli

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,39 @@ package main
 
 import (
 	"context"
-	"flag"
+	"github.com/urfave/cli/v2"
 	"mapgenie/analysis"
 	"mapgenie/changes"
 	"mapgenie/pkg/log"
 	"os"
+	// "github.com/urfave/cli/v2"
 )
 
 func main() {
+	app := &cli.App{
+		Name:      "mapgenie",
+		Usage:     "Generate code for converting one struct to another",
+		Args:      true,
+		ArgsUsage: "./path/... [path2 ...]",
+		Action: func(c *cli.Context) error {
+			if c.Args().Len() == 0 {
+				log.Errorf(c.Context, "Provide package patterns in arguments, for example: ./...")
+				cli.ShowAppHelpAndExit(c, 1)
+			}
+
+			targetFiles := analysis.FindTargetsInPackages(c.Context, c.Args().Slice()...)
+			changes.ApplyFilesChanges(c.Context, targetFiles)
+
+			return nil
+		},
+	}
+
 	ctx := context.Background()
 	ctx = log.Ctx(ctx, log.Warn, os.Stderr)
 
-	flag.Parse()
-	patterns := flag.Args()
-	if len(patterns) == 0 {
-		log.Errorf(ctx, "Provide package patterns in arguments, for example: ./...")
-	}
+	err := app.RunContext(ctx, os.Args)
 
-	targetFiles := analysis.FindTargetsInPackages(ctx, patterns...)
-	changes.ApplyFilesChanges(ctx, targetFiles)
+	if err != nil {
+		log.Errorf(ctx, "%s", err.Error())
+	}
 }
