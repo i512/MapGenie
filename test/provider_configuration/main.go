f91f4a7731654f8349d2a11e363085e9fb316082
Ilya Morozov <weugek@gmail.com>
2024-03-30 23:33:14 +0300

    [] provider-configuration

---
+++ test/provider_configuration/main.go
@@ -0,0 +1,9 @@
+package provider_configuration
+
+type A struct {
+	ID int64
+}
+
+type B struct {
+	ID []byte
+}
---
+++ test/provider_configuration/mapgen.go
@@ -0,0 +1,11 @@
+package provider_configuration
+
+import "mapgenie/entities"
+
+// mapgenie use
+
+// MapAB map this pls
+func MapAB(A) B {
+	entities.Fragment()
+	return B{}
+}
